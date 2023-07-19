7aae8d7d6b9edd25cb68646b25083d3bb95a5bf6
broody <broody@gmail.com>
2023-07-19 13:22:46 -0700

    chore(contracts): default auth script

---
+++ scripts/default_auth.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -euo pipefail
+pushd $(dirname "$0")/..
+
+export WORLD_ADDRESS="0x26065106fa319c3981618e7567480a50132f23932226a51c219ffb8e47daa84";
+
+# make sure all components/systems are deployed
+COMPONENTS=("Game" "Location" "Market" "Name" "Player" "Risks")
+SYSTEMS=("create_game" "join_game" "set_name" "travel" "buy" "sell")
+
+# check components
+for component in ${COMPONENTS[@]}; do
+    sozo component entity $component > /dev/null
+done
+
+# check systems
+for system in ${SYSTEMS[@]}; do
+    SYSTEM_OUTPUT=$(sozo system get $system)
+    if [[ "$SYSTEM_OUTPUT" == "0x0" ]]; then
+        echo "Error: Output of 'sozo system get $system' is 0x0"
+        exit 1
+    fi
+done
+
+# enable system -> component authorizations
+CREATE_GAME_COMPONENTS=("Game" "Location" "Market" "Name" "Player" "Risks")
+JOIN_GAME_COMPONENTS=("Game" "Location" "Player")
+SET_NAME_COMPONENTS=("Name")
+BUY_COMPONENTS=("Drug" "Market" "Name" "Player")
+SELL_COMPONENTS=("Drug" "Market" "Name" "Player")
+TRAVEL_COMPONENTS=("Location" "Player")
+
+for component in ${CREATE_GAME_COMPONENTS[@]}; do
+    sozo auth writer $component create_game > /dev/null
+done
+
+for component in ${JOIN_GAME_COMPONENTS[@]}; do
+    sozo auth writer $component join_game > /dev/null
+done
+
+for component in ${SET_NAME_COMPONENTS[@]}; do
+    sozo auth writer $component set_name > /dev/null
+done
+
+for component in ${BUY_COMPONENTS[@]}; do
+    sozo auth writer $component buy > /dev/null
+done
+
+for component in ${SELL_COMPONENTS[@]}; do
+    sozo auth writer $component sell > /dev/null
+done
+
+for component in ${TRAVEL_COMPONENTS[@]}; do
+    sozo auth writer $component travel > /dev/null
+done
+
+echo "Default authorizations have been successfully set."
