f94111bb97d5aeb1dd729a988837596f63cea582
broody <broody@gmail.com>
2023-08-24 15:16:35 -1000

    fix(contracts): fix auth script

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -2,20 +2,20 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-export WORLD_ADDRESS="0x26065106fa319c3981618e7567480a50132f23932226a51c219ffb8e47daa84";
+export WORLD_ADDRESS="0x49eea56f14aebfad0a3f73c530f0673084cc9c4b352965e685e52939221c389";
 
 # make sure all components/systems are deployed
-COMPONENTS=("Game" "Location" "Market" "Name" "Player" "Risks")
+COMPONENTS=("Game" "Market" "Name" "Player" "Risks")
 SYSTEMS=("create_game" "join_game" "set_name" "travel" "buy" "sell")
 
 # check components
 for component in ${COMPONENTS[@]}; do
-    sozo component entity $component
+    sozo component entity $component --world $WORLD_ADDRESS > /dev/null
 done
 
 # check systems
 for system in ${SYSTEMS[@]}; do
-    SYSTEM_OUTPUT=$(sozo system get $system)
+    SYSTEM_OUTPUT=$(sozo system get $system --world $WORLD_ADDRESS)
     if [[ "$SYSTEM_OUTPUT" == "0x0" ]]; then
         echo "Error: $system is not deployed"
         exit 1
@@ -23,35 +23,35 @@ for system in ${SYSTEMS[@]}; do
 done
 
 # enable system -> component authorizations
-CREATE_GAME_COMPONENTS=("Game" "Location" "Market" "Name" "Player" "Risks")
-JOIN_GAME_COMPONENTS=("Game" "Location" "Player")
+CREATE_GAME_COMPONENTS=("Game" "Market" "Name" "Player" "Risks")
+JOIN_GAME_COMPONENTS=("Game" "Player")
 SET_NAME_COMPONENTS=("Name")
 BUY_COMPONENTS=("Drug" "Market" "Name" "Player")
 SELL_COMPONENTS=("Drug" "Market" "Name" "Player")
-TRAVEL_COMPONENTS=("Location" "Player")
+TRAVEL_COMPONENTS=("Player")
 
 for component in ${CREATE_GAME_COMPONENTS[@]}; do
-    sozo auth writer $component create_game
+    sozo auth writer $component create_game --world $WORLD_ADDRESS
 done
 
 for component in ${JOIN_GAME_COMPONENTS[@]}; do
-    sozo auth writer $component join_game 
+    sozo auth writer $component join_game --world $WORLD_ADDRESS
 done
 
 for component in ${SET_NAME_COMPONENTS[@]}; do
-    sozo auth writer $component set_name 
+    sozo auth writer $component set_name --world $WORLD_ADDRESS
 done
 
 for component in ${BUY_COMPONENTS[@]}; do
-    sozo auth writer $component buy 
+    sozo auth writer $component buy --world $WORLD_ADDRESS
 done
 
 for component in ${SELL_COMPONENTS[@]}; do
-    sozo auth writer $component sell 
+    sozo auth writer $component sell --world $WORLD_ADDRESS
 done
 
 for component in ${TRAVEL_COMPONENTS[@]}; do
-    sozo auth writer $component travel 
+    sozo auth writer $component travel --world $WORLD_ADDRESS
 done
 
 echo "Default authorizations have been successfully set."
