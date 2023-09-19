abdf9548921709f9429a089afe8006f4eb30809d
notV4l <122404722+notV4l@users.noreply.github.com>
2023-09-19 22:27:14 +0200

    pwa test (#152)
    * pwa test
    
    * update images
    
    * update images

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* icons */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+      
+        {/* pwa */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#11ED83" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Head>
       <body>
         <Main />
         <NextScript />
