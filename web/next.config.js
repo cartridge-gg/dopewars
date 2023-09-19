554c5a4be72f0d159f5c3096735732e7781f2595
notV4l <imV4l@proton.me>
2023-09-19 15:48:12 +0200

    update images

--- web/next.config.js
+++ web/next.config.js
@@ -14,5 +14,5 @@ module.exports = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  // disable: process.env.NODE_ENV === 'development'
 })(nextConfig);
