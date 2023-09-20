27533cd0e3fafa5830a556437551ac3c19736b97
notV4l <imV4l@proton.me>
2023-09-20 11:29:27 +0200

    smaller text for mobile leaderboard

--- web/src/components/Leaderboard.tsx
+++ web/src/components/Leaderboard.tsx
@@ -120,7 +120,8 @@ const Leaderboard = ({
                 >
                   <HStack mr={3}>
                     <Text
-                      w="30px"
+                      w={["10px","30px"]}
+                      fontSize={["10px", "16px"]}
                       flexShrink={0}
                       // display={["none", "block"]}
                       whiteSpace="nowrap"
@@ -145,6 +146,7 @@ const Leaderboard = ({
                         maxWidth={["150px", "350px"]}
                         whiteSpace="nowrap"
                         overflow="hidden"
+                        fontSize={["12px", "16px"]}
                       >
                         {displayName}
                       </Text>
@@ -156,10 +158,13 @@ const Leaderboard = ({
                       backgroundRepeat="repeat-x"
                       flexGrow={1}
                       color="transparent"
+
                     >
                       {"."}
                     </Text>
-                    <Text flexShrink={0}>{formatCash(score.cash)}</Text>
+                    <Text flexShrink={0}
+                      fontSize={["12px", "16px"]}
+                    >{formatCash(score.cash)}</Text>
                   </HStack>
                 </ListItem>
               );
