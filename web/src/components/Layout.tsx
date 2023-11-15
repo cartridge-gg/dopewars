1219af3a636252b63926a4d431e9ce57f305dd64
notV4l <imV4l@proton.me>
2023-11-15 11:12:47 +0100

    fix ui scroll

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -116,7 +116,7 @@ const RightPanel = ({
       >
         {children}
         {!isSinglePanel && rigthPanelScrollable && (
-          <Box display="block" minH={isMobile ? "150px" : "90px"} h={isMobile ? "150px" : "90px"} w="full" />
+          <Box display="block" minH={isMobile ? "170px" : "90px"} h={isMobile ? "170px" : "90px"} w="full" />
         )}
       </VStack>
       {footer}
