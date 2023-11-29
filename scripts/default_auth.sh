7b2c730fa6801567df61f66fe32ec455dddf3c48
notV4l <imV4l@proton.me>
2023-11-29 17:17:57 +0100

    use sozo profile

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-if [ $# -ge 1 ] && [ $1 == "local" ]; then
-    export RPC_URL="http://localhost:5050"
+if [ $# -ge 1 ]; then
+    export PROFILE=$1
 else
-    export RPC_URL="https://api.cartridge.gg/x/ryo/katana"
+    export PROFILE="dev"
 fi
 
 export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
@@ -18,7 +18,8 @@ export SHOP_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | sel
 export RYO_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "ryo" ).address')
 
 echo "---------------------------------------------------------------------------"
-echo rpc : $RPC_URL
+echo profile : $PROFILE
+echo "---------------------------------------------------------------------------"
 echo world : $WORLD_ADDRESS
 echo " "
 echo lobby : $LOBBY_ADDRESS
@@ -38,38 +39,38 @@ SHOP_COMPONENTS=("Player" "Item" "Market")
 RYO_COMPONENTS=("RyoMeta" "Leaderboard")
 
 for component in ${LOBBY_COMPONENTS[@]}; do
-    sozo auth writer $component $LOBBY_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sozo -P $PROFILE auth writer $component $LOBBY_ADDRESS --world $WORLD_ADDRESS 
     sleep 0.1
 done
 
 for component in ${TRAVEL_COMPONENTS[@]}; do
-    sozo auth writer $component $TRAVEL_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sozo -P $PROFILE auth writer $component $TRAVEL_ADDRESS --world $WORLD_ADDRESS 
     sleep 0.1
 done
 
 for component in ${DECIDE_COMPONENTS[@]}; do
-    sozo auth writer $component $DECIDE_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sozo -P $PROFILE auth writer $component $DECIDE_ADDRESS --world $WORLD_ADDRESS 
     sleep 0.1
 done
 
 for component in ${TRADE_COMPONENTS[@]}; do
-    sozo auth writer $component $TRADE_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sozo -P $PROFILE auth writer $component $TRADE_ADDRESS --world $WORLD_ADDRESS 
     sleep 0.1
 done
 
 for component in ${SHOP_COMPONENTS[@]}; do
-    sozo auth writer $component $SHOP_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sozo -P $PROFILE auth writer $component $SHOP_ADDRESS --world $WORLD_ADDRESS 
     sleep 0.1
 done
 
 for component in ${RYO_COMPONENTS[@]}; do
-    sozo auth writer $component $RYO_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sozo -P $PROFILE auth writer $component $RYO_ADDRESS --world $WORLD_ADDRESS 
     sleep 0.1
 done
 
 echo "Default authorizations have been successfully set."
 
 echo "Initializing..."
-sozo execute $RYO_ADDRESS initialize
+sozo -P $PROFILE execute $RYO_ADDRESS initialize
 echo "Initialized!"
 
