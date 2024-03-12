35674c6b518dbf26aeba030803e4a4ee9ef253e6
SFYLL <santiagoflood@hotmail.fr>
2024-03-12 11:16:38 +0100

    Seismic client

--- web/src/seismic/client.ts
+++ web/src/seismic/client.ts
@@ -1,26 +1,62 @@
-import { Account } from "starknet";
-import { getNonce, signTypedDataStarknet } from "./lib/lib"; 
+import { Account, WeierstrassSignatureType } from "starknet";
+import { getNonce, stringifyBigInts} from "./lib/utils"; 
+import { signTypedDataStarknet } from "./lib/signature"; 
+import { tradeParametersActionDomain, tradeParametersActionTypeLabel, tradeParametersActionTypes } from "./lib/eip712.types";
+import axios from "axios";
 
-async function getTradeParameters(account: Account, seismic_url: string) {
+class SeismicClient {
+  private account: Account;
+  private seismic_url: string;
 
-    let senderNonce = await getNonce(account, seismic_url)
+  constructor(account: Account, seismic_url: string) {
+    this.account = account;
+    this.seismic_url = seismic_url;
+  }
+
+  /*
+  * Fetch trade parameters pre-image for the location that matches
+  * the player's
+  */
+  async getTradeParameters(game_id: string) {
+    let senderNonce = await getNonce(this.account, this.seismic_url);
 
     const tx = {
-        nonce: BigInt(senderNonce).toString(),
-        player_id: account.address,
-        game_id: '0',
+      nonce: BigInt(senderNonce).toString(),
+      player_id: this.account.address,
+      game_id: game_id.toString(),
+    };
 
-    }
+    const signature = await signTypedDataStarknet(
+      this.account,
+      tradeParametersActionTypes, 
+      tradeParametersActionTypeLabel, 
+      tradeParametersActionDomain, 
+      tx
+    ) as WeierstrassSignatureType;
 
-    const signature = await signTypedDataStarknet(account, tradeParametersActionTypes, tradeParametersActionTypeLabel, tradeParametersActionDomain, tx) as WeierstrassSignatureType;
-
-    const response = await axios.post(`${process.env.ENDPOINT}/trade/tradeParameters`, {
-        tx: stringifyBigInts(tx),
-        signature: stringifyBigInts(signature),
+    try {
+        const response = await axios.post(`${this.seismic_url}/trade/tradeParameters`, {
+            tx: stringifyBigInts(tx),
+            signature: stringifyBigInts(signature),
     });
-    if (response.status !== 200) {
-        throw new Error("Could not acquire data availability signature");
+
+    return response.data;
+
+    } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+        if (error.response.status === 404) {
+            if (error.response.data === "No market at home location") {
+                return undefined;
+            }
+        } else {
+            throw new Error(`HTTP error! status: ${error.response.status}`);
+        }
+    } else {
+        throw error; 
     }
-    console.log("response: ", response.data);
+    }
+  }
 }
 
+
+export default SeismicClient;
