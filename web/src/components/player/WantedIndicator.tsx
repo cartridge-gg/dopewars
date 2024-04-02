6cdbac07e90b45f3be097a4156518daec13636fb
Satyam Bansal <satyam@passageprotocol.com>
2024-04-02 14:27:52 +0530

    Change wanted indicator from percentage to category

--- web/src/components/player/WantedIndicator.tsx
+++ web/src/components/player/WantedIndicator.tsx
@@ -1,18 +1,20 @@
 import { HStack, StyleProps, Text } from "@chakra-ui/react";
-import { Siren } from "../icons";
+import { Alert, Siren } from "../icons";
 
 import { Tooltip } from "../common";
 import { blinkAnim } from "./HealthIndicator";
+import { formatThreatLevel } from "@/utils/ui";
 
 export const WantedIndicator = ({ wanted, ...props }: { wanted: number } & StyleProps) => {
   return (
     <Tooltip color="yellow.400" title="Wanted Level" text="Likelihood of encountering Cops or Gangs">
-      <HStack w="70px"
+      <HStack
+        w="70px"
         color={wanted > 68 ? "red" : wanted > 29 ? "yellow.400" : "neon.400"}
         animation={wanted >= 85 ? `${blinkAnim} infinite 0.5s linear` : "none"}
         {...props}
       >
-        <Siren /> <Text>{wanted}%</Text>
+        {wanted > 68 ? <Alert boxSize={21} /> : <Siren />} <Text>{formatThreatLevel(wanted)}</Text>
       </HStack>
     </Tooltip>
   );
