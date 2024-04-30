c0d8dc97574163468b56139d2639c42f780421f6
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-04-30 11:30:20 +0530

    Add static assets

--- web/src/pages/api/image.tsx
+++ web/src/pages/api/image.tsx
@@ -1,29 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextApiRequest, NextApiResponse } from "next";
 import sharp from "sharp";
 import satori from "satori";
 import { join } from "path";
 import * as fs from "fs";
-import IconImg from "./../../../public/images/image.svg";
-import Image from "next/image";
-// import { Image } from "@chakra-ui/react";
-// import Image from "next/image";
+import url from "util";
 
 const fontPath = join(process.cwd(), "public/fonts", "broken-console-broken-console-regular-400.ttf");
 const ppmondwestFontPath = join(process.cwd(), "public/fonts", "PPMondwest-Regular.otf");
 const dosWAgaFontPath = join(process.cwd(), "public/fonts", "DOS_VGA.ttf");
 
-const imagePath = join(process.cwd(), "public/images", "avatar.svg");
-console.log("font Path", fontPath);
+const cashboxImg = fs.readFileSync(join(process.cwd(), "public/images", "cashbox.png"));
+const playnowImg = fs.readFileSync(join(process.cwd(), "public/images", "playnow.png"));
+const avatarImg = fs.readFileSync(join(process.cwd(), "public/images", "avatar.png"));
+const cashIcon = fs.readFileSync(join(process.cwd(), "public/images", "cash_icon.png"));
+const calendarIcon = fs.readFileSync(join(process.cwd(), "public/images", "calendar.png"));
+const heartIcon = fs.readFileSync(join(process.cwd(), "public/images", "heart.png"));
+
 let fontData = fs.readFileSync(fontPath);
 let ppmondwestFontData = fs.readFileSync(ppmondwestFontPath);
 let dosWAgaFontData = fs.readFileSync(dosWAgaFontPath);
-let imageData = fs.readFileSync(imagePath);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const score = req.query["score"];
-    if (!score) {
-      return res.status(400).send("Missing Score");
+    const { rating, cash, day, hp, name } = req.query;
+
+    if (!rating || !cash || !day || !hp || !name) {
+      return res.status(400).send("Invalid query. query should have rating, cash, day, hp, and name.");
     }
 
     const svg = await satori(
@@ -41,7 +44,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           color: "#11ED83",
         }}
       >
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", flex: "1" }}>
           <p
             style={{
               textAlign: "center",
@@ -52,13 +55,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
               fontFamily: "broken-console , sans-serif",
             }}
           >
-            DOPE WARS
+            DOPE WARS:
           </p>
           <p style={{ textAlign: "center", fontSize: "48px", lineHeight: "1.2" }}>Roll Your Own</p>
 
-          <div style={{ display: "flex" }}>
-            <Image src={imagePath} alt="HI" width={200} height={300} />
-            hi
+          <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <img
+              src={`data:image/png;base64,${cashboxImg.toString("base64")}`}
+              alt="Cashbox"
+              width="120"
+              height="120"
+            />
+            <img
+              src={`data:image/png;base64,${playnowImg.toString("base64")}`}
+              alt="Play Now"
+              width="258"
+              height="64"
+              style={{ marginTop: "40px" }}
+            />
           </div>
         </div>
 
@@ -70,11 +84,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             padding: 20,
             border: "1px solid #1C291C",
             borderRadius: "6px",
+            flex: 1,
           }}
         >
-          {/* <Image src="/public/images/dealer.png" alt="cash suitcase" width={200} height={200} /> */}
-          <Image src="/images/events/smoking_gun.gif" alt="rip" />
-          <Image src="/images/landing/main.png" alt="hi" width={200} height={200} />
+          <img src={`data:image/png;base64,${avatarImg.toString("base64")}`} alt="Avatar" width="50%" />
 
           <div
             style={{
@@ -87,11 +100,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 fontSize: "56px",
               }}
             >
-              Click_save
+              {name}
             </h2>
             <div
               style={{
-                paddingBottom: "3rem",
+                paddingBottom: "1rem",
                 borderBottom: "2px solid #1C291C",
               }}
             >
@@ -102,34 +115,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
               style={{
                 display: "flex",
                 flexDirection: "column",
-                gap: "2rem",
-                marginTop: "3rem",
+                gap: "1rem",
+                marginTop: "1rem",
                 fontFamily: "dos-waga , sans-serif",
               }}
             >
               <li style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-                <img
-                  src="https://graduate.northeastern.edu/resources/wp-content/uploads/sites/4/2019/09/iStock-1150384596-2.jpg"
-                  alt="cash suitcase"
-                  width="30"
-                />
-                <p style={{ margin: "0" }}>{score}</p>
+                <img src={`data:image/png;base64,${cashIcon.toString("base64")}`} alt="Cash" width="40" height="40" />
+                <p style={{ margin: "0" }}>{cash}</p>
               </li>
               <li style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
                 <img
-                  src="https://graduate.northeastern.edu/resources/wp-content/uploads/sites/4/2019/09/iStock-1150384596-2.jpg"
-                  alt="cash suitcase"
-                  width="30"
+                  src={`data:image/png;base64,${calendarIcon.toString("base64")}`}
+                  alt="Calendar"
+                  width="40"
+                  height="40"
                 />
-                <p style={{ margin: "0" }}>Day 24</p>
+                <p style={{ margin: "0" }}>Day {day}</p>
               </li>
               <li style={{ display: "flex", alignItems: "center", gap: "1rem" }}>
-                <Image
-                  src="https://graduate.northeastern.edu/resources/wp-content/uploads/sites/4/2019/09/iStock-1150384596-2.jpg"
-                  alt="cash suitcase"
-                  width="30"
-                />
-                <p style={{ margin: "0" }}>55 HP</p>
+                <img src={`data:image/png;base64,${heartIcon.toString("base64")}`} alt="Heart" width="40" height="40" />
+                <p style={{ margin: "0" }}>{hp} HP</p>
               </li>
             </ul>
           </div>
